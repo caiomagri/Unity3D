fc4e520c57210da5755639242f4214af724a96be
caiomagri <kaioeduardofm@gmail.com>
2020-03-29 13:22:10 -0300

    [ADD] add score in header and falling plataform for next level

--- Assets/Scripts/Apple.cs
+++ Assets/Scripts/Apple.cs
@@ -8,6 +8,7 @@ public class Apple : MonoBehaviour {
 	private CircleCollider2D circleCollider;
 
 	public GameObject collected;
+	public int Score;
 
 	// Use this for initialization
 	void Start () {
@@ -23,6 +24,9 @@ public class Apple : MonoBehaviour {
 			spriteRenderer.enabled = false;
 			circleCollider.enabled = false;
 			collected.SetActive(true);
+
+			GameController.instance.totalScore += Score;
+			GameController.instance.UpdateScoreText();
 
 			Destroy(gameObject, 0.25f);
 		}
